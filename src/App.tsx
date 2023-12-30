c22666e5badf40eb48701f8f3f9ce1346a5effbe
rondindon <ronnie.rybarik@gmail.com>
2023-12-30 02:33:11 +0100

    search bar done

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ function App() {
   const [pokedexes, setPokedexes] = useState<Array<any>>([]);
   const [selectedPokemonPokedex, setSelectedPokemonPokedex] = useState<Array<any>>([]);
   const [isPokedexSelected, setIsPokedexSelected] = useState<boolean>(false);
+  const [pokemonSearch, setPokemonSearch] = useState<string>("");
 
   useEffect(() => {
     axios.get(pokedexURL).then((response) => {
@@ -36,20 +37,37 @@ function App() {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
+  const filteredPokemon = selectedPokemonPokedex.filter((pokemon) =>
+    pokemon.pokemon_species.name.toLowerCase().includes(pokemonSearch.toLowerCase())
+  );
+
   return (
     <>
       <Navbar onTitleClick={handleTitleClick} />
 
       {isPokedexSelected ? (
-        <div className="mon-list">
-          {selectedPokemonPokedex.map((pokemon, index) => (
-            <PokemonEntry
-              key={index}
-              entryNumber={pokemon.entry_number}
-              speciesName={pokemon.pokemon_species.name}
-              speciesUrl={pokemon.pokemon_species.url}
-            />
-          ))}
+          <div className="mons">
+
+            <div className="search-bar">
+              <input
+                type="text"
+                placeholder="Search Pokemon"
+                value={pokemonSearch}
+                onChange={(e) => setPokemonSearch(e.target.value)}
+              />
+            </div>
+
+            <div className="mon-list">
+              {filteredPokemon.map((pokemon, index) => (
+                <PokemonEntry
+                  key={index}
+                  entryNumber={pokemon.entry_number}
+                  speciesName={pokemon.pokemon_species.name}
+                  speciesUrl={pokemon.pokemon_species.url}
+                />
+              ))}
+          </div>
+
         </div>
       ) : (
         <div className="dex-list">
